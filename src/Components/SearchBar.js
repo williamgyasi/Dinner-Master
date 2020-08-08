80055daad6f904cbc812e8158db881fd685a8bcf
William Gyasi <kwabenagyasi63@gmail.com>
2020-08-08 00:56:09 +0000

    Text Input

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {StyleSheet,View} from 'react-native'
-import {Dimensions} from 'react-native'
+import {Dimensions,TextInput} from 'react-native'
 import {Icon} from "native-base"
 const width=Dimensions.get("screen").width
 const height=Dimensions.get("screen").height
@@ -8,8 +8,9 @@ const height=Dimensions.get("screen").height
 
 const SearchBar=()=>{
     return(
-        <View style={[styles.background]}>
-            <Icon name="search" color={"black"}   type="Feather" fontSize={20} />
+        <View style={styles.background}>
+            <Icon type="FontAwesome" name="search" active={false} style={{color:'black',fontSize:20,marginLeft:10}} />
+            <TextInput
         </View>
     )
 
@@ -18,7 +19,7 @@ const SearchBar=()=>{
 
 const styles=StyleSheet.create({
     background:{
-        backgroundColor:"red",
+        backgroundColor:"gray",
         maxWidth:width,
         maxHeight:height,
         width:width,
