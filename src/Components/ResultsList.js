c56063ee05757a6e515f17e887021cc2e2a922b3
William Gyasi <kwabenagyasi63@gmail.com>
2020-08-09 01:29:35 +0000

    Reusable Star Component

--- src/Components/ResultsList.js
+++ src/Components/ResultsList.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import {View,Button,Text} from 'native-base'
-import {StyleSheet} from 'react-native'
+import {StyleSheet,FlatList,} from 'react-native'
 
 const ResultList=(props)=>{
     const{title,results}=props
+
+    const renderItem=({item})=>{
+        return(
+            <View>
+                <Text>
+                    {item.name}
+                </Text>
+            </View>
+        )
+
+    }
     return(
         <View>
             <Text 
@@ -12,7 +23,15 @@ const ResultList=(props)=>{
                 fontWeight:"bold",
             }}>{title}</Text>
 
-            <Text>COMPONENT HAS {results.length} </Text>
+            <FlatList
+            data={results}
+            horizontal={true}
+            keyExtractor={(result)=>result.id}
+            renderItem={renderItem}
+            />
+
+
+           
             
         </View>
     )
