574e79ed7a03eb76b700139198fc0d1bdef913bf
William Gyasi <kwabenagyasi63@gmail.com>
2020-08-13 03:28:38 +0000

    Restaurant Photos

--- src/Screens/ResultDetailScreen.js
+++ src/Screens/ResultDetailScreen.js
@@ -1,11 +1,45 @@
-import React from 'react'
-import {View} from 'react-native'
+import React,{useEffect,useState} from 'react'
+import {View,FlatList,Image} from 'react-native'
 import {Container,Text} from 'native-base'
+import yelp from '../Api/Yelp'
 const ResultDetailScreen=({route,navigation})=>{
-    const {id}=route.params
+    const [restaurant,setRestaurant]=useState(null)
+    const{id}=route.params
+    console.log(restaurant)
+
+    useEffect(()=>{
+        getResult(id)
+    },[])
+
+    const getResult=(id)=>{
+        yelp.get(`/${id}`)
+        .then(result=>setRestaurant(result.data))
+        .catch(e=>alert(e))
+    }
+
+
+    const _renderItem=({item})=>{
+        return(
+            <Image style={{width:100,height:"100%"}} source={{uri:item}} />
+        )
+    }
+
+
+
+
+    if(!restaurant){
+        return null
+    }
     return(
         <View>
-            <Text>{id}</Text>
+            <Text>{restaurant.name}</Text>
+            <FlatList 
+            data={restaurant.photos}
+            extraData={restaurant}
+            keyExtractor={(item,index)=>item.id}
+            renderItem={_renderItem}
+
+            />
         </View>
 
     )
